04183d67ab27eb2a5a4012a613d9ba1021329e1e
unknown <najmiddinnasriddinov5@gmail.com>
2024-03-30 23:49:33 +0500

    All Done

---
+++ index.js
@@ -0,0 +1,50 @@
+    // 1-MASALA
+// let n = 100;
+// let counterJ;
+
+// for (let i = 2; i <= n; i++) {
+//     counterJ = 0;
+//     for (let j = 1; j <= i; j++) {
+//         if (i % j == 0) {
+//             counterJ++;
+//         }
+//     }
+
+//     if (counterJ == 2) {
+//         console.log(i);
+//     }
+// }
+
+
+
+    // 2-MASALA
+// let username = prompt("Iltimos, foydalanuvchi nomini kiriting:");
+// let password = prompt("Iltimos, parolni kiriting:");
+
+
+// if (username === "Najmiddin" && password === "Nasrddinovvv0809") {
+//     alert("Kirish muvaffaqiyatli amalga oshirildi!");
+//     document.write(`Username: ${username}`);
+//     document.write(`<br>`);
+//     document.write(`Password: ${password}`);
+// } else {
+//     alert("Kirish uchun noto'g'ri ma'lumotlar kiritildi!");
+// }
+
+
+
+    // 3-MASALA
+// let inform = prompt();
+// console.log(`Informatsiya: ${inform}`);
+
+
+
+    // 4-MASALA
+// let a = prompt('Birinchi sonni kiriting');
+// let b = prompt('Ikkinchi sonni kiriting');
+
+// for (let i = a; i <= b; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
